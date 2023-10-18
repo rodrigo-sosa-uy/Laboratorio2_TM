a789ee68fbbb0110fbe8637b61f239c1113a1734
rodrigo-sosa-uy <144396760+rodrigo-sosa-uy@users.noreply.github.com>
2023-10-18 11:51:05 -0300

    Lab 2 PWM - ADC - v18.10.23-11.45
    Versión del código 18.10.23-11.45
    -Definición temporal de actuadores.
    -Creación de la máquina de estado.
    -Adición de lectura de fotoresistencia.
    -Creación de funciones para estados.

--- main.c
+++ main.c
@@ -43,9 +43,46 @@ PIN 4 - LED VERDE
 #define velMid 333		// Velocidad Media
 #define velSeg 100		// Velocidad de Seguridad
 
+#define BOTON PORTBx
+
+#define MA1 PORTBx
+#define MA2 PORTBx
+#define MB1 PORTBx
+#define MB2 PORTBx
+
+#define LED_V PORTx
+#define LED_R PORTx
+
+unsigned short fotores;
+unsigned int tiempo = 0;
+
+typedef enum{
+OFF,
+ADELANTE,
+DERECHA,
+IZQUIERDA,
+ATRAS,
+SAFETY_CAR
+} STATE;
+
+typedef struct{
+void (*func)();
+} FSM;
+
+STATE estado = OFF;
+
+FSM Autito[] = {
+{F_OFF},
+{F_ADELANTE},
+{F_DERECHA},
+{F_IZQUIERDA},
+{F_ATRAS},
+{F_SAFETY_CAR}
+};
+
 void configPORT(){
-	DDRC = 0x0F;
-	DDRD = 0xF0;
+	DDRC = 0x0F;	// STANDBY
+	DDRD = 0xF0;	// STANDBY
 }
 
 void configPWM(){
@@ -56,13 +93,181 @@ void configPWM(){
 	TCCR1B = 0b00011001;
 }
 
+void configADC() {
+	ADMUX |= (1 << REFS0);
+
+	ADCSRA |= (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+}
+
+unsigned short readADC(unsigned char canal){
+	ADMUX = (ADMUX & 0xF0) | (canal & 0x0F);
+
+	ADCSRA |= (1 << ADSC);
+
+	while (ADCSRA & (1 << ADSC));
+	
+	return ADC;
+}
+
+void fotoresistor(){
+	fotores = readADC(x); // cambiar
+	
+	if(fotores > x){ // cambiar
+		PORTB |= (1 << x); // cambiar
+	} else{
+		PORTB &= ~(1 << x); // cambiar
+	}
+	_delay_ms(100);
+}
+
+void F_OFF(){
+	PORTx &= ~(1 << MA1);
+	PORTx &= ~(1 << MA2);
+	
+	PORTx &= ~(1 << MB1);
+	PORTx &= ~(1 << MB2);
+	
+	PORTx &= ~(1 << LED_R);
+	PORTx &= ~(1 << LED_V);
+	
+	if((PINx & (1 << BOTON)){
+		estado = ADELANTE;
+	}
+}
+
+void F_ADELANTE(){
+	OCR1A = velMax;
+	
+	PORTx |= (1 << MA1);
+	PORTx &= ~(1 << MA2);
+
+	PORTx |= (1 << MB1);
+	PORTx &= ~(1 << MB2);
+	
+	PORTx |= (1 << LED_V);
+	PORTX &= ~(1 << LED_R);
+	
+	fotoresistor();
+	
+	//tiempo :)
+	
+	if((PINx & (1 << BOTON)){
+		estado = OFF;
+	} else if(/* tiempo */){
+		estado = x;
+		tiempo = 0;
+	}
+}
+
+void F_DERECHA(){
+	OCR1A = velMax;
+	
+	PORTx |= (1 << MA1);
+	PORTx &= ~(1 << MA2);
+
+	OCR1A = velDiff;
+
+	PORTx |= (1 << MB1);
+	PORTx &= ~(1 << MB2);
+	
+	PORTx |= (1 << LED_V);
+	PORTx &= ~(1 << LED_R);
+	
+	fotoresistor();
+	
+	//tiempo :)
+	
+	if((PINx & (1 << BOTON)){
+		estado = OFF;
+	} else if(/* tiempo */){
+		estado = x;
+		tiempo = 0;
+	}
+}
+
+void F_IZQUIERDA(){
+	OCR1A = velDiff;
+	
+	PORTx |= (1 << MA1);
+	PORTx &= ~(1 << MA2);
+
+	OCR1A = velMax;
+
+	PORTx |= (1 << MB1);
+	PORTx &= ~(1 << MB2);
+	
+	PORTx |= (1 << LED_V);
+	PORTx &= ~(1 << LED_R);
+	
+	fotoresistor();
+	
+	//tiempo :)
+	
+	if((PINx & (1 << BOTON)){
+		estado = OFF;
+	} else if(/* tiempo */){
+		estado = x;
+		tiempo = 0;
+	}
+}
+
+void F_ATRAS(){
+	OCR1A = velMid;
+	
+	PORTx &= ~(1 << MA1);
+	PORTx |= (1 << MA2);
+
+	PORTx &= ~(1 << MB1);
+	PORTx |= (1 << MB2);
+	
+	PORTx &= ~(1 << LED_V);
+	PORTx |= (1 << LED_R);
+	
+	fotoresistor();
+	
+	//tiempo :)
+	
+	if((PINx & (1 << BOTON)){
+		estado = OFF;
+	} else if(/* tiempo */){
+		estado = x;
+		tiempo = 0;
+	}
+}
+
+void F_SAFETY_CAR(){
+	OCR1A = velSeg;
+	
+	PORTx |= (1 << MA1);
+	PORTx &= ~(1 << MA2);
+
+	PORTx |= (1 << MB1);
+	PORTx &= ~(1 << MB2);
+	
+	PORTx |= (1 << LED_V);
+	PORTX &= ~(1 << LED_R);
+	
+	fotoresistor();
+	
+	//tiempo :)
+	
+	if((PINx & (1 << BOTON)){
+		estado = OFF;
+	} else if(/* tiempo */){
+		estado = x;
+		tiempo = 0;
+	}
+}
+
 int main(void){
+	
     configPWM();
 	
 	configPORT();
 	
+	configADC();
+	
     while(1){
-		//
+		(*Autito[estado].func)();
     }
 }
-
